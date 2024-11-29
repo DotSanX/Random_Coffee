d59fa8f39e494755193ac6219fce0ed10b2c7f20
timothy <caveiroussight@gmail.com>
2024-11-29 18:55:08 +0600

    id check

--- lib/bot.ts
+++ lib/bot.ts
@@ -1,10 +1,12 @@
 import { Bot } from "https://deno.land/x/grammy@v1.32.0/mod.ts";
 import { commands } from "./router/commands.ts";
 
-export const bot = new Bot(Deno.env.get("BOT_TOKEN") || ""); // export нужен, чтобы воспользоваться ботом в другом файле
+export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
 
 for (const command in commands) {
     bot.command(
         command, commands[command]
     );
 }
+
+const users = {}
--- lib/router/handlers.ts
+++ lib/router/handlers.ts
@@ -1,3 +1,3 @@
-const handlers = {
-    
+export const handlers = {
+
 }
