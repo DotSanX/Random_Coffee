a0dc008349c87b2d9c097ae94ea8d382a48b537d
timothy <caveiroussight@gmail.com>
2024-12-01 12:25:04 +0600

    fix

--- lib/bot.ts
+++ lib/bot.ts
@@ -1,52 +1,50 @@
-import {Bot, Context} from "https://deno.land/x/grammy@v1.32.0/mod.ts";
+import { Bot, Context } from "https://deno.land/x/grammy@v1.32.0/mod.ts";
 interface UserInfo {
-  state: string, 
-  info: Record<string, string | number | []>
+  state: string;
+  info: Record<string, string | number | []>;
 }
 type MyContext = Context & {
-  config: UserInfo
-}
+  config: UserInfo;
+};
 export const bot = new Bot<MyContext>(Deno.env.get("BOT_TOKEN") || "");
 
 bot.use(
   async (ctx, next) => {
     ctx.config = {
       state: "",
-      info: {}
-    }
-    await next()
-  }
-)
+      info: {},
+    };
+    await next();
+  },
+);
 
 bot.command("start", async (ctx) => {
-  if (ctx.msg.from.id in ctx.config.users) {
-    await ctx.reply("ты уже тупа юзер");
-  } else {
-    await ctx.reply(
-      "Привет!👋🏻 \nВижу, ты тут впервые. Я - бот Коффи☕️. С моей помощью ты сможешь пообщаться с людьми, которым интересно то же, что и тебе!",
-    );
-    await ctx.reply(
-      "🤔 А как зовут тебя? \n <b>Учти, что твое имя увидят другие пользователи.</b>",
-      { parse_mode: "HTML" },
-    );
-    ctx.config.state = "setName";
-  }
+  await ctx.reply(
+    "Привет!👋🏻 \nВижу, ты тут впервые. Я - бот Коффи☕️. С моей помощью ты сможешь пообщаться с людьми, которым интересно то же, что и тебе!",
+  );
+  await ctx.reply(
+    "🤔 А как зовут тебя? \n <b>Учти, что твое имя увидят другие пользователи.</b>",
+    { parse_mode: "HTML" },
+  );
+  ctx.config.state = "setName";
 });
 
 bot.on("message", async (ctx) => {
-  console.log(ctx.config.state)
+  console.log(ctx.config.state);
   if (ctx.config.state) {
     const state = ctx.config.state;
     switch (state) {
       case "setName":
-        ctx.config.state = ctx.msg.text;
-        ctx.reply("Отличное имя, " + ctx.config.userInfo.name + "!");
+        ctx.config.state = ctx.msg.text || "";
+        ctx.reply("Отличное имя, " + ctx.config.info.name + "!");
         ctx.reply("Кстати, сколько тебе лет?");
         ctx.config.state = "setAge";
         break;
       case "setAge":
-        ctx.config.userInfo.age = Number(ctx.msg.text);
-        ctx.reply("Отлично! 🤩 Отправь мне местоположение, рядом с которым тебе будет удобно встретиться");
+        ctx.config.info.age = Number(ctx.msg.text);
+        ctx.reply(
+          "Отлично! 🤩 Отправь мне местоположение, рядом с которым тебе будет удобно встретиться",
+        );
         ctx.config.state = "setInterests";
         break;
 
