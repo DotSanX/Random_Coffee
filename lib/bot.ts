10aba5fa3302ace8e801558e42fef251c44615d1
timothy <caveiroussight@gmail.com>
2024-12-02 18:03:03 +0600

    starting database work

--- lib/bot.ts
+++ lib/bot.ts
@@ -1,4 +1,3 @@
-
 import {
   Bot,
   Context,
@@ -12,7 +11,6 @@ interface UserInfo {
   interests: string[];
   geo: string;
   time: string;
-
 }
 type MyContext = Context & {
   config: UserInfo;
@@ -20,11 +18,10 @@ type MyContext = Context & {
 
 const database = await Deno.openKv();
 const curruser = 12144141;
-await database.set(["users", curruser], "admin")
+await database.set(["users", curruser], "admin");
 
 //объявил бота
 export const bot = new Bot<MyContext>(Deno.env.get("BOT_TOKEN") || "");
-
 
 let state = ""; // эта переменная нужна для обработки состояния - устанавливает ли пользователь имя или, например, ожидает встречи
 // Список states на данный момент:
@@ -33,28 +30,34 @@ let state = ""; // эта переменная нужна для обработ�
 // setInterests - установка интересов
 // setGeo - установка геопозиции
 // setTime - установка времени
-// Pending - состояние ожидания 
+// Pending - состояние ожидания
 // Searching - состояние поиска
 
 // info будет нужна для сохранения инфо пользователя в бд (или получения) - представляет из себя набор данных о пользователе
 const info: UserInfo = {
   name: "",
   age: 0,
-  interests : [],
+  interests: [],
   geo: "",
-  time: ""
+  time: "",
 };
 
 // будущий middleware !пригодится для бд!
 bot.use(
-  async (ctx, next) => {
-    // ctx.config = {
-    // };
-    await next();
-  },
+  // async (ctx, next) => {
+  //   // ctx.config = {
+  //   // };
+  //   await next();
+  // },
 );
 
 bot.command("start", async (ctx) => { // бот получает команду /start
+  if (curruser in database.get(["users"])) {
+    ctx.reply("test");
+  } else {
+    ctx.reply("damn")
+    ctx.reply(`${(await database.get(["users"])).value}`)
+  }
   await ctx.reply(
     "Привет!👋🏻 \nВижу, ты тут впервые. Я - бот Коффи☕️. С моей помощью ты сможешь пообщаться с людьми, которым интересно то же, что и тебе!",
   );
@@ -66,31 +69,37 @@ bot.command("start", async (ctx) => { // бот получает команду 
 });
 
 // клавиатура для подтверждения интересов
-const yesOrNo = new InlineKeyboard().text("Да✅", "interestsDone").text("Нет❌", "interestsNotDone")
+const yesOrNo = new InlineKeyboard().text("Да✅", "interestsDone").text(
+  "Нет❌",
+  "interestsNotDone",
+);
 
 //обработка подтверждения интересов
-bot.callbackQuery("interestsDone", async ctx=>{
-  await ctx.deleteMessage()
-  await ctx.reply("Отлично!")
+bot.callbackQuery("interestsDone", async (ctx) => {
+  await ctx.deleteMessage();
+  await ctx.reply("Отлично!");
   state = "pending";
-})
-bot.callbackQuery("interestsNotDone", async ctx=>{
-  await ctx.deleteMessage()
-  await ctx.reply("Хорошо, напиши еще увлечений!")
+});
+bot.callbackQuery("interestsNotDone", async (ctx) => {
+  await ctx.deleteMessage();
+  await ctx.reply("Хорошо, напиши еще увлечений!");
   state = "setInterests";
-})
+});
 
 bot.on("message", async (ctx) => {
   if (state) { // при непустом state
     switch (state) {
       case "setName":
-
-        if (typeof ctx.msg.text !== "string" ||
-          /[0-9_.*^%$#@!]/.test(ctx.msg.text)) {
-          await ctx.reply("Извини, но имя должно быть текстом, не содержащим цифр или спецсимволов!");
+        if (
+          typeof ctx.msg.text !== "string" ||
+          /[0-9_.*^%$#@!]/.test(ctx.msg.text)
+        ) {
+          await ctx.reply(
+            "Извини, но имя должно быть текстом, не содержащим цифр или спецсимволов!",
+          );
           return;
         }
-        
+
         info.name = ctx.msg.text || ""; //сохраняем в переменную
         await ctx.reply("Приятно познакомиться, " + info.name + "!");
         await ctx.reply("Кстати, сколько тебе лет?");
@@ -102,7 +111,7 @@ bot.on("message", async (ctx) => {
           await ctx.reply("Извини, но нужно ввести возраст числом!");
           return;
         }
-        
+
         info.age = Number(ctx.msg.text);
         await ctx.reply(
           "Отлично! 🤩 Отправь мне местоположение, рядом с которым тебе будет удобно встретиться",
@@ -113,18 +122,16 @@ bot.on("message", async (ctx) => {
         state = "setGeo";
         break;
       case "setGeo":
-
-
         if (!ctx.msg.location) {
-          await ctx.reply("🤔 Я не понял. Пожалуйста, отправь мне местоположение");
+          await ctx.reply(
+            "🤔 Я не понял. Пожалуйста, отправь мне местоположение",
+          );
           return;
         }
-        
 
         info.geo =
           `${ctx.msg.location?.latitude}, ${ctx.msg.location?.longitude}`; // записываем геопозицию в виде: ширина, долгота
         await ctx.reply(
-
           "😎 А теперь расскажи мне немного о себе. Перечисли через запятую свои хобби и увлечения!",
         );
         state = "setInterests";
@@ -137,13 +144,12 @@ bot.on("message", async (ctx) => {
           }
         }
         await ctx.reply(
-
-          "🏆 Вот список твоих интересов:"
+          "🏆 Вот список твоих интересов:",
         );
         await ctx.reply(
-          info.interests.toString()
+          info.interests.toString(),
         );
-        await ctx.reply("Это все?", {reply_markup: yesOrNo});// смотри bot.callbackQuery
+        await ctx.reply("Это все?", { reply_markup: yesOrNo }); // смотри bot.callbackQuery
         break;
       default:
         break;
