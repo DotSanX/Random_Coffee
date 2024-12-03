d71fbfb21dca1279a74e24006073a606d89c69a9
Dotsan <dotsan_20@mail.ru>
2024-12-03 17:38:23 +0300

    гео

---
+++ lib/functions.ts
@@ -0,0 +1,22 @@
+import { MyContext } from "./interfaces.ts";
+import { database, info } from "./bot.ts";
+import { acceptKeyboard } from "./keyboards.ts";
+
+export async function reviewProfile(ctx: MyContext) {
+  await setState("review");
+  await ctx.reply("Вот, как тебя увидят другие пользователи:");
+  await ctx.reply(
+    `${info.name}, ${info.age}\n` +
+      `Список интересов: ${info.interests.toString()}`,
+  );
+  await ctx.reply("Геопозиция района, где будет удообно встретиться:");
+  await ctx.replyWithLocation(info.geo.latitude, info.geo.longitiute);
+  await ctx.reply("Все верно?", {
+    reply_markup: acceptKeyboard,
+  });
+}
+
+export async function setState(state: string) {
+  info.state = state;
+  await database.set(["users", info.id, "state"], state);
+}
---
+++ lib/interfaces.ts
@@ -0,0 +1,17 @@
+import { Context } from "https://deno.land/x/grammy@v1.32.0/mod.ts";
+
+export interface UserInfo {
+  id: number;
+  name: string;
+  age: number;
+  interests: string[];
+  geo: Record<string, number>;
+  time: string;
+  state: string;
+  rating: number;
+  done: boolean;
+}
+
+export type MyContext = Context & {
+  config: UserInfo;
+};
