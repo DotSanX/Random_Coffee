5b33240c701213034a930130cba400d7f69e7f0f
Dotsan <dotsan_20@mail.ru>
2024-12-06 14:00:00 +0300

    Тест

--- lib/functions.ts
+++ lib/functions.ts
@@ -1,8 +1,8 @@
-import { MyContext } from "./interfaces.ts";
-import { database, info } from "./bot.ts";
+import { Context } from "https://deno.land/x/grammy@v1.32.0/mod.ts";
+import { info, users } from "./bot.ts";
 import { acceptKeyboard } from "./keyboards.ts";
 
-export async function reviewProfile(ctx: MyContext) {
+export async function reviewProfile(ctx: Context) {
   await setState("review");
   await ctx.reply("Вот, как тебя увидят другие пользователи:");
   await ctx.reply(
@@ -10,7 +10,7 @@ export async function reviewProfile(ctx: MyContext) {
       `Список интересов: ${info.interests.toString()}`,
   );
   await ctx.reply("Геопозиция района, где будет удообно встретиться:");
-  await ctx.replyWithLocation(info.geo.latitude, info.geo.longitiute);
+//   await ctx.replyWithLocation(info.geo.latitude, info.geo.longitiute);
   await ctx.reply("Все верно?", {
     reply_markup: acceptKeyboard,
   });
@@ -18,5 +18,5 @@ export async function reviewProfile(ctx: MyContext) {
 
 export async function setState(state: string) {
   info.state = state;
-  await database.set(["users", info.id, "state"], state);
+  await users.update({ state: info.state }).eq("tg_id", info.id);
 }
